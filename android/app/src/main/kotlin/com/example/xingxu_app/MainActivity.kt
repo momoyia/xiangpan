a654d05c5d37fb3a34b381d22c9439c6966a398c
admin <admin@admindeMacBook-Pro-2.local>
2026-06-22 22:25:14 -0700

    1.3.1

--- android/app/src/main/kotlin/com/example/xingxu_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.xingxu_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/models/RotateTensorCosineStack.dart
+++
@@ -1,9 +0,0 @@
-class FloatSpecifyScalabilityManager {
-  const FloatSpecifyScalabilityManager({
-    required this.label,
-    required this.color,
-  });
-
-  final String label;
-  final int color;
-}
